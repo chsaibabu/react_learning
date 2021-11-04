895f56c5bcc72e4c6c3b0486445e312dbcb8faa1
Ch Saibabu <ch.saibabu.961@gmail.com>
2021-11-04 15:10:29 +0530

    added

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,24 @@ import React from "react";
 
 function Header(){
     return(
-        <div>
+        <div className="header">
             <h1>Head</h1>
+            <ul>
+             <li>
+             <a href="#home">
+                 Home
+             </a>
+             <a href="#">
+                 Home
+             </a>
+             <a href="#">
+                 Home
+             </a>
+             <a href="#">
+                 Home
+             </a>
+             </li>
+           </ul> 
             <p> this is a react build in the project </p>
         </div>
     )
