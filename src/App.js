3c7dc440f9a4b26654b00b7ac0068d3d687ab733
Ch Saibabu <ch.saibabu.961@gmail.com>
2021-11-09 13:23:58 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './Header.js';
 import './App.css';
 import Footer from './Footer';
+import Main from './Main.js';
 
 function App() {
   return (
     <div className="App"> 
         <Header/>
+        <Main/>
         <Footer/>
     </div>
   );
