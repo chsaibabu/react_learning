65d69880af2937d61d07772376aa244b4abe6d6c
Ch Saibabu <ch.saibabu.961@gmail.com>
2021-11-20 16:37:42 +0530

    added

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,12 +3,21 @@ import React from 'react';
 const HelloWorld = () => {
 
   function sayHello() {
-    alert('Hello, World!');
+    alert('Welcome!');
   }
   
   return (
-    <button onClick={sayHello}>Click me
+    <div className="login">
+    <h1>Login</h1>
+    <p>Welcome! Enter your user name and password to access your account.</p>
+    <form>
+      <input text="type" placeholder="Username" />
+      <input text="type" placeholder="password" />
+      <span>-----Forgent your password ?-----</span>
+    </form>
+    <button onClick={sayHello}>Login
     </button>
+    </div>
   );
 };
 
